2049cf99ea07924f4e86edea668ba76bfb4fb9f4
Yuan Fu <fuyuanx@gmail.com>
2015-01-24 14:21:26 -0800

    gui and game management

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -12,6 +12,10 @@ public class Apple : MonoBehaviour {
 	void Update () {
 		if ( transform.position.y < bottomY ) {
 			Destroy( this.gameObject );
-		}
+			// Get a reference to the ApplePicker component of Main Camera
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>(); // 1
+			// Call the public AppleDestroyed() method of apScript
+			apScript.AppleDestroyed();     
+		}                                 // 2
 	}
 }
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 
 public class Basket : MonoBehaviour {
-
+	public GUIText        scoreGT;
 	void Start () {
+		// Find a reference to the ScoreCounter GameObject
+		GameObject scoreGO = GameObject.Find("ScoreCounter");               // 2
+		// Get the GUIText Component of that GameObject
+		scoreGT = scoreGO.GetComponent<GUIText>();                          // 3
+		// Set the starting number of points to 0
+		scoreGT.text = "0";
 	}
 	
 	// Update is called once per frame
@@ -29,6 +35,16 @@ public class Basket : MonoBehaviour {
 		if ( collidedWith.tag == "Apple" ) {                                // 4
 			Destroy( collidedWith );
 		}
-	}
 
+		// Parse the text of the scoreGT into an int
+		int score = int.Parse( scoreGT.text );                              // 4
+		// Add points for catching the apple
+		score += 100;
+		// Convert the score back to a string and display it
+		scoreGT.text = score.ToString();
+		if (score > HighScore.score) {
+			HighScore.score = score;
+		}
+	}
+	
 }
